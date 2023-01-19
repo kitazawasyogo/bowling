446733632be267d026fd5dfd51eee708a781cc57
北澤　翔吾 <86754665+kitazawasyogo@users.noreply.github.com>
2023-01-19 17:47:35 +0900

    Initial commit

---
+++ Bowling.Test/BowlingTest.vb
@@ -0,0 +1,11 @@
+﻿Imports NUnit.Framework
+
+Public Class BowlingTest
+
+    <Test()> Public Sub NUnitでのサンプルコード()
+        Dim actual As Integer = 2 + 3
+
+        Assert.That(actual, [Is].EqualTo(5))
+    End Sub
+
+End Class
---
+++ Bowling/Bowling.vb
@@ -0,0 +1,3 @@
+﻿Public Class Bowling
+
+End Class
