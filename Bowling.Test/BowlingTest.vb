e293c795142e55008a33fe45872e29f1319f4c2a
KITAZAWA Syogo <kitazawa@daniel-soft.com>
2023-01-20 10:53:10 +0900

    Scoreの引数の配列の長さが異なる場合、引数例外を投げる処理追加
    間違えて既存のメソッドに変更を加えてしまった為、既存のメソッドを元に戻し新しくテストメソッドを追加
    
    指定のメソッド名でないメソッド名を使用していた為、指定のメソッド名に修正
    
    メソッド名に合わせたコメントの修正とSummary以外のコメントの追加
    
    戻り値の設定方法の改修
    
    Scoreの引数の配列の長さが異なる場合、引数例外を投げる処理追加
    
    IF文の条件が分かりづらい為分かりやすいコードに修正
    
    テストが正しい動きをしていなかった為正しいコードに修正

--- Bowling.Test/BowlingTest.vb
+++ Bowling.Test/BowlingTest.vb
@@ -2,10 +2,54 @@
 
 Public Class BowlingTest
 
+
     <Test()> Public Sub NUnitでのサンプルコード()
+
         Dim actual As Integer = 2 + 3
 
         Assert.That(actual, [Is].EqualTo(5))
+
     End Sub
 
+
+    <Test()> Public Sub 点数を返すメソッドのテスト()
+
+        Dim pinsArray As Integer() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
+        Dim actual As Integer = Bowling.Score(pinsArray)
+
+        Assert.That(actual, [Is].EqualTo(231))
+
+    End Sub
+
+    <Test()> Public Sub 配列の長さが少ない場合例外を投げるテスト()
+
+        Try
+
+            Dim pinsArray As Integer() = {1, 2, 3, 4, 5}
+            Bowling.Score(pinsArray)
+            Assert.Fail()
+
+        Catch ex As ArgumentException
+
+            Assert.That(ex.Message, [Is].EqualTo("値が有効な範囲にありません。"))
+
+        End Try
+
+    End Sub
+
+    <Test()> Public Sub 配列の長さが多い場合例外を投げるテスト()
+
+        Try
+
+            Dim pinsArray As Integer() = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
+            Bowling.Score(pinsArray)
+            Assert.Fail()
+
+        Catch ex As ArgumentException
+
+            Assert.That(ex.Message, [Is].EqualTo("値が有効な範囲にありません。"))
+
+        End Try
+
+    End Sub
 End Class
--- Bowling/Bowling.vb
+++ Bowling/Bowling.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Bowling
 
+    ''' <summary>
+    ''' 点数を返すメソッド
+    ''' </summary>
+    ''' <param name="pins">得点の入った配列</param>
+    ''' <returns>得点の合計値</returns>
+    Public Shared Function Score(pins As Integer()) As Integer
+
+        If pins.Length <> 21 Then
+            Throw New ArgumentException
+        End If
+
+        Return (pins.Sum())
+
+    End Function
+
 End Class
